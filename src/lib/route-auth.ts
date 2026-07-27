2a96ed40de65d903a9c136cdfac0e53361dde564
Arunima Tripathy <rue@mightyacorn.com>
2026-07-27 18:33:33 -0400

    fix: linting error

--- src/lib/route-auth.ts
+++ src/lib/route-auth.ts
@@ -2,15 +2,15 @@ import { redirect } from "@tanstack/react-router";
 import { getLoggedInUserProfile, type Profile } from "./auth";
 
 export async function requireAuth(): Promise<Profile> {
-    const profile = await getLoggedInUserProfile();
-    if (!profile) throw redirect ({ to: "/login" });
-    return profile;
+  const profile = await getLoggedInUserProfile();
+  if (!profile) throw redirect({ to: "/login" });
+  return profile;
 }
 
 export async function requireRole(role: Profile["role"]): Promise<Profile> {
-    const profile = await requireAuth();
-    if (profile.role !== role){
-        throw redirect({ to: profile.role == "admin" ? "/admin" : "/dashboard" });
-    }
-    return profile;
+  const profile = await requireAuth();
+  if (profile.role !== role) {
+    throw redirect({ to: profile.role == "admin" ? "/admin" : "/dashboard" });
+  }
+  return profile;
 }
