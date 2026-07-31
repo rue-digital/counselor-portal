d6d4f5b1b497a6b98b2456d17c987d7555764b5f
Arunima Tripathy <rue@mightyacorn.com>
2026-07-30 21:42:12 -0400

    chore: uncomment redirect

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -10,12 +10,12 @@ import { toast } from "sonner";
 import { redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
-  // beforeLoad: async () => {
-  //   const profile = await getLoggedInUserProfile();
-  //   if (profile) {
-  //     throw redirect({ to: profile.role == "admin" ? "/admin" : "/dashboard" });
-  //   }
-  // },
+  beforeLoad: async () => {
+    const profile = await getLoggedInUserProfile();
+    if (profile) {
+      throw redirect({ to: profile.role == "admin" ? "/admin" : "/dashboard" });
+    }
+  },
   head: () => ({
     meta: [
       { title: "Sign in — DARN Counselor Portal" },
